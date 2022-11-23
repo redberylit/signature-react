99f049134e19fef26908fd9c908b4a9f96d57df6
srinathredbery <srinath@redberylit.com>
2022-11-23 08:39:54 +0530

    add home layout

--- src/data/sidebar-nav-items.js
+++ src/data/sidebar-nav-items.js
@@ -1,40 +1,40 @@
 export default function() {
   return [
     {
-      title: "Blog Dashboard",
+      title: "Dashboard",
       to: "/blog-overview",
       htmlBefore: '<i class="material-icons">edit</i>',
       htmlAfter: ""
     },
     {
-      title: "Blog Posts",
+      title: "Assign",
       htmlBefore: '<i class="material-icons">vertical_split</i>',
       to: "/blog-posts",
     },
     {
-      title: "Add New Post",
+      title: "Add New Document",
       htmlBefore: '<i class="material-icons">note_add</i>',
       to: "/add-new-post",
     },
     {
-      title: "Forms & Components",
+      title: "Review Document",
       htmlBefore: '<i class="material-icons">view_module</i>',
       to: "/components-overview",
-    },
-    {
-      title: "Tables",
-      htmlBefore: '<i class="material-icons">table_chart</i>',
-      to: "/tables",
-    },
-    {
-      title: "User Profile",
-      htmlBefore: '<i class="material-icons">person</i>',
-      to: "/user-profile-lite",
-    },
-    {
-      title: "Errors",
-      htmlBefore: '<i class="material-icons">error</i>',
-      to: "/errors",
-    }
+     }
+    // {
+    //   title: "Tables",
+    //   htmlBefore: '<i class="material-icons">table_chart</i>',
+    //   to: "/tables",
+    // },
+    // {
+    //   title: "User Profile",
+    //   htmlBefore: '<i class="material-icons">person</i>',
+    //   to: "/user-profile-lite",
+    // },
+    // {
+    //   title: "Errors",
+    //   htmlBefore: '<i class="material-icons">error</i>',
+    //   to: "/errors",
+    // }
   ];
 }
